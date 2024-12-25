054b8107605cbcdc6d71b681f49f24a3af35ffba
Aman Kumar <trulyaman25@gmail.com>
2024-12-25 18:02:50 +0530

    Updated Room Logic

--- src/room/roomPage.jsx
+++ src/room/roomPage.jsx
@@ -1,36 +1,88 @@
-import { useParams } from "react-router-dom";
-import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
-function RoomPage() {
-    const { roomID } = useParams();
+function randomID(len) {
+  let result = '';
+  const chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP';
+  const maxPos = chars.length;
+  len = len || 5;
 
-    const myMeeting = async(element) => {
-        const appID = 973992499;
-        const serverSecret = "2ff4a753c672adbe7b642b731280f20d";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Aman Kumar");
-        const zc = ZegoUIKitPrebuilt.create(kitToken);
-        zc.joinRoom({
-            container: element,
-            sharedLinks: [
-                {
-                    name: 'Copy Link',
-                    url: `wellwisevctesting.vercel.app/room/${roomID}`
-                }
-            ],
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall
-            },
-            showAudioVideoSettingsButton: true,
-            showMyCameraToggleButton: true, 
-            showScreenSharingButton: true
-        })
-    }
-
-    return (
-        <>
-            <div ref={myMeeting}></div>
-        </>
-    )
+  for (let i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
 }
 
-export default RoomPage;
+// get token
+function generateToken(tokenServerUrl, appID, userID) {
+  // Obtain the token interface provided by the App Server
+  return fetch(tokenServerUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      app_id: appID,
+      user_id: userID,
+    }),
+  }).then(async (res) => {
+    const result = await res.text();
+    return result;
+  });
+}
+
+export function getUrlParams(url = window.location.href) {
+  const urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+  const roomID = getUrlParams().get('roomID') || randomID(5);
+  const userID = randomID(5);
+  const userName = randomID(5);
+
+  const myMeeting = React.useCallback(async (element) => {
+    if (!element) return;
+
+    // Generate token
+    const token = await generateToken(
+      'https://preview-uikit-server.zegotech.cn/api/token',
+      2013980891,
+      userID
+    );
+
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForProduction(
+      2013980891,
+      token,
+      roomID,
+      userID,
+      userName
+    );
+
+    // Create instance object from token
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    // Start the call
+    zp.joinRoom({
+      container: element,
+      showPreJoinView: false,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url: `${window.location.origin}${window.location.pathname}?roomID=${roomID}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // Change to OneONoneCall for 1-on-1 calls
+      },
+    });
+  }, [roomID, userID, userName]);
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
